b75ee4ee23ce32c80aef0a1d2b0717025037a614
Baibua <baibua.1215@sis.edu>
2024-01-23 12:24:00 +0700

    :)

--- Is all sunk.py
+++ Is all sunk.py
@@ -1,6 +1,6 @@
 
 def is_all_sunk(Player):
-    if ship5 and ship4 and ship3 and ship2:
+    if Player[0] == "F" and Player[1] == "F" and Player[2] == "F" and Player[3] == "F":
         return True
     else:
         return False
