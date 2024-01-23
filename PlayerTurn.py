9c82cb8f99872f343a5d20aad505c3dfa62d44b8
Baibua <baibua.1215@sis.edu>
2024-01-23 12:39:47 +0700

    :)

--- PlayerTurn.py
+++ PlayerTurn.py
@@ -1,8 +1,11 @@
+from Checkifhitship import check_hit
 
 def PlayerTurn (Opp_Board):
     print("Your turn. Enter a co-ordinate to hit")
     print(Opp_Board)
-    location = input()
-
-    return location
+    print("Enter the row, 1-10")
+    row = input()
+    print("Enter the column, 1-10")
+    column = input()
+    check_hit(Opp_Board, row, column)
 
