2ff0a078ccb47699a78fa1e4f68294d4b724717b
Baibua <baibua.1215@sis.edu>
2024-01-23 11:49:15 +0700

    :)

--- InitialiseVariables.py
+++ InitialiseVariables.py
@@ -1,5 +1,7 @@
 #sets up a 10x10 array of strings
 player1board = [['' for i in range(10)] for j in range(10)]
 player2board = [['' for i in range(10)] for j in range(10)]
+player1board_view = [['' for i in range(10)] for j in range(10)]
+player2board_view = [['' for i in range(10)] for j in range(10)]
 #print(board)
 
