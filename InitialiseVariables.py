7a03c3499163d1fdf8477bbac3faa3928605fad2
Baibua <baibua.1215@sis.edu>
2024-01-23 12:25:05 +0700

    :)

--- InitialiseVariables.py
+++ InitialiseVariables.py
@@ -3,6 +3,7 @@ player1board = [['' for i in range(10)] for j in range(10)]
 player2board = [['' for i in range(10)] for j in range(10)]
 player1board_view = [['' for i in range(10)] for j in range(10)]
 player2board_view = [['' for i in range(10)] for j in range(10)]
-
+player1ships = ["Ship2", "Ship3", "Ship4", "Ship5"]
+player2ships = ["Ship2", "Ship3", "Ship4", "Ship5"]
 #print(board)
 
