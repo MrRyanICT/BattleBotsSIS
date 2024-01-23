911644281952a8085b0f63d11beca28bc2f66088
Baibua <baibua.1215@sis.edu>
2024-01-23 12:35:03 +0700

    :)

--- PlayerTurn.py
+++ PlayerTurn.py
@@ -1,7 +1,8 @@
 
-def PlayerTurn (Player, Opp_Board):
+def PlayerTurn (Opp_Board):
     print("Your turn. Enter a co-ordinate to hit")
     print(Opp_Board)
     location = input()
+
     return location
 
