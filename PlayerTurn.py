c377df46161aec6fc67930887014a83b93cb949d
MrRyanICT <100187608+MrRyanICT@users.noreply.github.com>
2024-01-24 09:30:45 +0700

    Update PlayerTurn.py

--- PlayerTurn.py
+++ PlayerTurn.py
@@ -3,8 +3,11 @@ from Checkifhitship import check_hit
 def PlayerTurn (Opp_Board):
     print("Your turn. Enter a co-ordinate to hit")
     print(Opp_Board)
-    print("Enter the row, 1-10")
-    row = input()
+    while True:
+        print("Enter the row, 1-10")
+        row = int(input())
+        if row < 1 or row > 10:
+            print('Error. Choose a number between 1 and 10')
     print("Enter the column, 1-10")
     column = input()
     check_hit(Opp_Board, row, column)
