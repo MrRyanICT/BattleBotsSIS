794274e0cc73081bf15ce43269edbbe7d23f9b1c
thanyarat1377 <fay.1377@sis.edu>
2024-02-06 11:38:20 +0700

    :)

--- PlayersTurn.py
+++ PlayersTurn.py
@@ -1,4 +1,4 @@
-from CheckIfHitShip import check_hit
+#from CheckIfHitShip import check_hit
 from InitialiseVariables import player1board_view, player2board_view, player1ships, player2ships
 from ChangeToNumber import changetonum
 
@@ -10,13 +10,17 @@ def PlayerTurn (Opp_Board):
 
     print("Your turn. Enter a co-ordinate to hit")
     print(Opp_Board)
-    while True:
-        print("Enter the row, 1-10")
+
+    print("Enter the row, 1-10")
+    row = int(input())
+    while row < 1 or row > 10:
+        print('Error. Choose a number between 1 and 10')
         row = int(input())
-        if row < 1 or row > 10:
-            print('Error. Choose a number between 1 and 10')
+
     print("Enter the column, A-J. Capital letter please")   #I suggest using A-J
     column = input()
     column = changetonum(column)
-    check_hit(Opp_Board, row, column, playerships)
+    #check_hit(Opp_Board, row, column, playerships)
+
+PlayerTurn(player1board_view)
 
