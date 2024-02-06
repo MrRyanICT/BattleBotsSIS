6c493d1da9d3fe79a623f75fa39fe46550b05b9b
chan-1213 <mac.1213@sis.edu>
2024-02-06 12:10:14 +0700

    o

--- WinOrLose.py
+++ WinOrLose.py
@@ -1,8 +1,9 @@
+from Isallsunk import is_all_sunk
 PlayerShipsA = ['F','F','F','F']
 
 def winorlose():
     #A is true, B is false
-    if is_all_sunk(PlayerShipsA) == True:
+    if is_all_sunk(PlayerShipsA):
         print('player A wins!')
     else:
         print('player B wins!')
