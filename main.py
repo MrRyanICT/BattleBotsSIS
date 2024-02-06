19b45550941703ad34a9d6ae98cec95f7f16c8af
Baibua <baibua.1215@sis.edu>
2024-02-06 12:36:45 +0700

    :)

--- main.py
+++ main.py
@@ -6,7 +6,3 @@ print('Hi')
 print("Battle Bots!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
 player1board, player2board = setboard()
 
-print()
-while not is_all_sunk:
-    PlayerTurn(player2board_view)
-    PlayerTurn(player1board_view)
