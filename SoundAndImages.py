b7a77cd0cffa5873a209f798d7b0cec5f6611499
athipong912 <hill.912@sis.edu>
2024-03-04 12:36:49 +0700

    hi

--- SoundAndImages.py
+++ SoundAndImages.py
@@ -12,3 +12,23 @@ from playsound import playsound
 # 	sys.exit()  # to exit from all the processes
 # cv2.destroyAllWindows()  # destroy all windows
 playsound('Resources/sounds/Lose.wav')
+
+def PlayMedia(name):
+    if name == "enemy down":
+        playsound('Resources/sounds/Enemy Down.wav')
+        print('ENEMY SHIP HIT!')
+    elif name == "explosion":
+        playsound('Resources/sounds/explosion.wav')
+        print('BOOM!')
+    elif name == "lose":
+        playsound('Resources/sounds/Lose.wav')
+        print('YOU LOST!')
+    elif name == "miss":
+        playsound('Resources/sounds/miss.wav')
+        print('YOU MISSED!')
+    elif name == "ship down":
+        playsound('Resources/sounds/Ship down.wav')
+        print('YOUR SHIP HAS SUNK!')
+
+
+
