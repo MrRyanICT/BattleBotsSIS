68f2d7ec7463d3f4450e3d2c40fa9bf47c594f72
thamawat2051 <save.888boy@gmail.com>
2024-03-11 12:21:03 +0700

    ...

--- SoundAndImages.py
+++ SoundAndImages.py
@@ -2,13 +2,26 @@
 from playsound import playsound
 #playsound('Resources/sounds/explosion.wav')
 
-# import sys  # to access the system
-# #import from Python Packages
-# import cv2
-# img1 = cv2.imread("Resources/images/explosion.png", cv2.IMREAD_ANYCOLOR)
-# while True:
-# 	cv2.imshow("Explosion", img1)
-# 	cv2.waitKey(0)
-# 	sys.exit()  # to exit from all the processes
-# cv2.destroyAllWindows()  # destroy all windows
-playsound('Resources/sounds/Lose.wav')
+import sys  # to access the system
+#import from Python Packages
+import cv2
+img1 = cv2.imread("Resources/images/explosion.png", cv2.IMREAD_ANYCOLOR)
+while True:
+	cv2.imshow("Explosion", img1)
+	cv2.waitKey(0)
+	sys.exit()  # to exit from all the processes
+cv2.destroyAllWindows()  # destroy all windows
+#from CheckIfHitShip import check_hit
+#from Isallsunk import is_all_sunk
+def PlaySound(soundname):
+    if soundname == "Win":
+       playsound('Resources/sounds/Win.wav')
+    if soundname == "Lose":
+        playsound('Resources/Sounds/Lose.wav')
+    if soundname == "Hit":
+        playsound('Resources/Sounds/hit.wav')
+    if soundname == "miss":
+        playsound('Resources/Sounds/miss.wav')
+
+#def Background(Image)
+
