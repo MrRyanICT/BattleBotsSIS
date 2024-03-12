eadb3bbae912aa3bd404db63d2ed9658ce3913b0
Baibua <baibua.1215@sis.edu>
2024-03-12 12:26:40 +0700

    :)

--- main.py
+++ main.py
@@ -4,7 +4,17 @@ from PlayersTurn import PlayerTurn
 
 print('Hi')
 print("Battle Bots!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-player1board, player2board, destroyer1, destroyer2, submarine1, submarine2, battleship1, battleship2, carrier1, carrier2 = setboard(player1board, player2board)
+#player1board, player2board, destroyer1, destroyer2, submarine1, submarine2, battleship1, battleship2, carrier1, carrier2 = setboard(player1board, player2board)
+player1board = [['S', 'S', '', '', '', '', '', '', '', ''], ['S', 'S', 'S', '', '', '', '', '', '', ''], ['S', 'S', 'S', 'S', '', '', '', '', '', ''], ['S', 'S', 'S', 'S', 'S', '', '', '', '', ''], ['', '', '', '', '', '', '', '', '', ''], ['', '', '', '', '', '', '', '', '', ''], ['', '', '', '', '', '', '', '', '', ''], ['', '', '', '', '', '', '', '', '', ''], ['', '', '', '', '', '', '', '', '', ''], ['', '', '', '', '', '', '', '', '', '']]
+player2board = [['S', 'S', '', '', '', '', '', '', '', ''], ['S', 'S', 'S', '', '', '', '', '', '', ''], ['S', 'S', 'S', 'S', '', '', '', '', '', ''], ['S', 'S', 'S', 'S', 'S', '', '', '', '', ''], ['', '', '', '', '', '', '', '', '', ''], ['', '', '', '', '', '', '', '', '', ''], ['', '', '', '', '', '', '', '', '', ''], ['', '', '', '', '', '', '', '', '', ''], ['', '', '', '', '', '', '', '', '', ''], ['', '', '', '', '', '', '', '', '', '']]
+destroyer1 = [0, 0, 0, 1]
+destroyer2 = [0, 0, 0, 1]
+submarine1 = [1, 0, 1, 1, 1, 2]
+submarine2 = [1, 0, 1, 1, 1, 2]
+battleship1 = [2, 0, 2, 1, 2, 2, 2, 3]
+battleship2 = [2, 0, 2, 1, 2, 2, 2, 3]
+carrier1 = [3, 0, 3, 1, 3, 2, 3, 3, 3, 4]
+carrier2 = [3, 0, 3, 1, 3, 2, 3, 3, 3, 4]
 
 while True:
     PlayerTurn(player2board_view, destroyer1, destroyer2, submarine1, submarine2, battleship1, battleship2, carrier1, carrier2)
